76a48494eb702a14ded5ae4b1750fcb631396135
shahip008 <shahip008@gmail.com>
2026-03-22 16:20:33 +0530

    Completed till section 9

--- shahimart-ui/src/Component/Header.jsx
+++ shahimart-ui/src/Component/Header.jsx
@@ -1,12 +1,40 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShoppingBasket,
+  faTags,
+  faMoon,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   console.log("entering into header");
   const navLinkClass =
-    "text-center text-lg font-primary font-semibold text-primary py-2";
+    "text-center text-lg font-primary font-semibold text-primary py-2 dark:text-light hover:text-dark dark:hover:text-lighter";
+
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") === "dark" ? "dark" : "light";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme == "light" ? "dark" : "light";
+
+      localStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
+  };
+
   return (
-    <header className="border-b border-gray-300 sticky top-0 z-20 bg-gray-100">
+    <header className="border-b border-gray-300 dark:border-gray-600 sticky top-0 z-20 bg-normalbg dark:bg-darkbg">
       <div className="flex items-center justify-between mx-auto max-w-[1152px] px-6 py-4">
         <a href="/" className={navLinkClass}>
           <FontAwesomeIcon icon={faTags} className="h-8 w-8" />
@@ -15,6 +43,16 @@ const Header = () => {
         </a>
 
         <nav className="flex items-center py-2 z-10">
+          <button
+            className="flex items-center justify-center mx-3 w-8 h-8 rounded-full border border-primary dark:border-light transition duration-300 hover:bg-gray-300 dark:hover:bg-gray-600"
+            aria-label="Toggle-theme"
+            onClick={toggleTheme}
+          >
+            <FontAwesomeIcon
+              icon={theme === "dark" ? faMoon : faSun}
+              className="w-4 h-4 dark:text-light text-primary"
+            />
+          </button>
           <ul className="flex space-x-6">
             <li>
               <a href="/" className={navLinkClass}>
@@ -38,7 +76,10 @@ const Header = () => {
             </li>
             <li>
               <a href="/cart" className="text-primary py-2">
-                <FontAwesomeIcon icon={faShoppingBasket} />
+                <FontAwesomeIcon
+                  icon={faShoppingBasket}
+                  className="dark:text-light"
+                />
               </a>
             </li>
             I
